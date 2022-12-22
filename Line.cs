66e86048d0e5b27908f17f9417bd13521e355ae7
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-22 19:22:18 +0530

    Length of Line by Cartesian system

---
+++ Line.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison_p3
+{
+    public class Line
+    {
+        public double LengthOfLine(int x1,int y1,int x2,int y2)
+        {
+            double X1 = Math.Pow((x2 - x1), 2);
+            double Y1 = Math.Pow((y2 - y1), 2);
+            double length = Math.Sqrt(X1 + Y1);
+            Console.WriteLine(length);
+            return length;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LineComparison_p3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Line line = new Line();
+            
+            double length_1st_line = line.LengthOfLine(20, 30, 40, 50);
+
+        }
+    }
+}
