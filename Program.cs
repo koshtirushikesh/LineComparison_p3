b1744f5dee98d59c45cbe2806099f682aff7227c
“koshtirushikesh” <koshtirushikesh3@gmail.com>
2022-12-22 19:38:21 +0530

    check equality of two lines based on the end points

--- Program.cs
+++ Program.cs
@@ -7,7 +7,18 @@
             Line line = new Line();
             
 
-            double length_1st_line = line.LengthOfLine(20, 30, 40, 50);
+            double length_1st_line = line.LengthOfLine(20, 30, 40, 50); 
+            double length_2nd_line = line.LengthOfLine(30, 40, 30, 50);
+
+
+            if (length_1st_line == length_2nd_line)
+            {
+                Console.WriteLine("1st Line: {0} and 2nd line: {1} is equal in length ", length_1st_line, length_2nd_line);
+            }
+            else
+            {
+                Console.WriteLine("1st Line: {0} and 2nd line: {1} is not equal in length ", length_1st_line, length_2nd_line);
+            }
 
         }
     }
